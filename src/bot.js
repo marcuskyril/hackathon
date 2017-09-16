8ef798b7ef39cfcb70bf4d9e576042729052ef44
Marcus Kyril Lim <marcus.lim.2014@sis.smu.edu.sg>
2017-09-16 02:08:59 +0200

    fixed merge conflicts.. again

--- src/bot.js
+++ src/bot.js
@@ -1,18 +1,11 @@
 'use strict';
 const Builder = require('botbuilder');
-<<<<<<< HEAD
 const { Logger, Messages, Endpoint, Intents} = require('./shared/const');
-=======
-const { Logger, Messages, Endpoint, Intents } = require('./shared/const');
->>>>>>> 7db171162604e568a53ee639bb98c0f2ee42bf42
 //const JsonStorage = require('./data/BotJsonStore');
 
 
 const BaseDialog = require('./dialogs/base-dialog');
-<<<<<<< HEAD
-=======
 const SearchDialog = require('./dialogs/search-dialog');
->>>>>>> 7db171162604e568a53ee639bb98c0f2ee42bf42
 
 
 
@@ -55,17 +48,7 @@ class YourBot {
     constructor(connector, recognizer) {
         this.core = new Builder.UniversalBot(connector, (session, args) => {
             //TODO: Default Handler that will be called if no dialog or other handler triggers
-<<<<<<< HEAD
-            //session.send(Messages.Greeting);
         });
-
-        //this.core.recognizer(recognizer);
-=======
-            session.send(Messages.Greeting);
-        });
-
-        // this.core.recognizer(recognizer);
->>>>>>> 7db171162604e568a53ee639bb98c0f2ee42bf42
         //this.core.set('storage', new JsonStorage()); //TODO: Remove this line to use memory storage (State will be lost after shutdown)
         this.init();
     }
@@ -73,7 +56,6 @@ class YourBot {
 
     init() {
         //TODO: Add your dialogs to the bot
-<<<<<<< HEAD
         this.core.dialog(BaseDialog.getName(), [
           BaseDialog.greet,
           BaseDialog.askForName,
@@ -84,18 +66,10 @@ class YourBot {
         ])
         .triggerAction({ matches: /^(hello)/i })
         .cancelAction('CancelPlaceAdding', 'Okay', { matches: /^(cancel|nevermind|abort)/i });
-    }
-
-
-=======
-        this.core.dialog(BaseDialog.getName(), [BaseDialog.askUserForName, BaseDialog.greetUser])
-            .triggerAction({ matches: /^(hello)/i })
-            .cancelAction('CancelPlaceAdding', 'Okay', { matches: /^(cancel|nevermind|abort)/i });
 
         this.core.dialog(SearchDialog.getName(), [SearchDialog.askUserForLocation, SearchDialog.retrieveRecommendations, SearchDialog.validateRecommendations])
             .triggerAction({ matches: /^(search)/i })
     }
->>>>>>> 7db171162604e568a53ee639bb98c0f2ee42bf42
 }
 
 module.exports = {
